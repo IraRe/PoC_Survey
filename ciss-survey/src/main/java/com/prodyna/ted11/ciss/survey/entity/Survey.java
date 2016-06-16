d604cff5225f456c6b8838abfde669ba0abae6ae
Iryna Feuerstein <iryna.feuerstein@prodyna.com>
2016-06-16 16:29:35 +0200

    Entities added

--- ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/entity/Survey.java
+++ ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/entity/Survey.java
@@ -1,14 +1,45 @@
 package com.prodyna.ted11.ciss.survey.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Survey {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long Id;
+    private Long id;
+	
+	private String name;
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	private List<Question> questions;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+	
 }
