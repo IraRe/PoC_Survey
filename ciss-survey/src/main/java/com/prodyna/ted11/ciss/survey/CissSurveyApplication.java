13d1bde6c304ffb04b39b4591f4a27ce49cf4d29
Stanislav Mayburd <stanislav.mayburd@prodyna.com>
2016-06-16 12:19:38 +0200

    This is where it all begins...

---
+++ ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/CissSurveyApplication.java
@@ -0,0 +1,12 @@
+package com.prodyna.ted11.ciss.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CissSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CissSurveyApplication.class, args);
+	}
+}
