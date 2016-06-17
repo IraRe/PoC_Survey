e9f9d0edebf0623ef6eb2aebcb9ac71412427bc1
Iryna Feuerstein <iryna.feuerstein@prodyna.com>
2016-06-17 09:34:09 +0200

    REST interface created. GET all survey works.

--- ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/controller/SurveyRestController.java
+++ ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/controller/SurveyRestController.java
@@ -1,8 +1,13 @@
 package com.prodyna.ted11.ciss.survey.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prodyna.ted11.ciss.survey.entity.Survey;
 import com.prodyna.ted11.ciss.survey.repository.SurveyRepository;
 
 @RestController
@@ -11,7 +16,11 @@ public class SurveyRestController {
 	@Autowired
 	private SurveyRepository surveyRepository;
 	
-	public void getSurvey() {
-		
+	@RequestMapping(method=RequestMethod.GET, value="/survey")
+	public List<Survey> getAllSurveys() {
+		List<Survey> allSurveys = surveyRepository.findAll();
+		return allSurveys;
 	}
+	
+	
 }
