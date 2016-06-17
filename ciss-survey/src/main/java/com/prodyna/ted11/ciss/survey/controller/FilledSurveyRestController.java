bf25984b21dffda8df08224435fce93cb4257bd8
Iryna Feuerstein <iryna.feuerstein@prodyna.com>
2016-06-17 12:26:55 +0200

    GET current filled survey by userId added

--- ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/controller/FilledSurveyRestController.java
+++ ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/controller/FilledSurveyRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prodyna.ted11.ciss.survey.entity.FilledSurvey;
@@ -30,7 +31,7 @@ public class FilledSurveyRestController {
 	private AnswerRepository answerRepository;
 	
 	@RequestMapping(method=RequestMethod.GET, value="/filled")
-	public List<FilledSurvey> getAllSurveys() {
+	public List<FilledSurvey> getAllFilledSurveys() {
 		List<FilledSurvey> allFilledSurveys = new ArrayList<>();
 		List<Survey> allSurveys = surveyRepository.findAll();
 		for (Survey survey : allSurveys) {
@@ -42,7 +43,7 @@ public class FilledSurveyRestController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/filled")
-	public HttpEntity<FilledSurvey> saveSurvey(@RequestBody FilledSurvey filledSurvey) {
+	public HttpEntity<FilledSurvey> saveFilledSurvey(@RequestBody FilledSurvey filledSurvey) {
 		Collection<UserAnswer> userAnswers = filledSurvey.getUserAnswers();
 		List<UserAnswer> savedAnswers = answerRepository.save(userAnswers);
 		FilledSurvey savedSurvey = new FilledSurvey(filledSurvey.getSurvey(), savedAnswers);
@@ -50,11 +51,21 @@ public class FilledSurveyRestController {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, value="/filled/{id}")
-	public HttpEntity<FilledSurvey> getFulledSurveyById(@PathVariable Long id) {
+	public HttpEntity<FilledSurvey> getFilledSurveyById(@PathVariable Long id) {
 		FilledSurvey filledSurvey = new FilledSurvey();
 		Survey survey = surveyRepository.findById(id);
 		filledSurvey.setSurvey(survey);
 		List<UserAnswer> userAnswers = answerRepository.findAllBySurveyName(survey.getName());
+		filledSurvey.setUserAnswers(userAnswers);
+		return new ResponseEntity<FilledSurvey>(filledSurvey, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/filled/current")
+	public HttpEntity<FilledSurvey> getCurrentFilledSurveyByUserId(@RequestParam String userId) {
+		FilledSurvey filledSurvey = new FilledSurvey();
+		Survey survey = surveyRepository.findTopByOrderByIdDesc();
+		filledSurvey.setSurvey(survey);
+		List<UserAnswer> userAnswers = answerRepository.findBySurveyNameAndUserId(survey.getName(), userId);
 		filledSurvey.setUserAnswers(userAnswers);
 		return new ResponseEntity<FilledSurvey>(filledSurvey, HttpStatus.OK);
 	}
--- ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/repository/AnswerRepository.java
+++ ciss-survey/src/main/java/com/prodyna/ted11/ciss/survey/repository/AnswerRepository.java
@@ -9,4 +9,6 @@ import com.prodyna.ted11.ciss.survey.entity.UserAnswer;
 public interface AnswerRepository extends JpaRepository<UserAnswer, Long> {
 
 	public List<UserAnswer> findAllBySurveyName(String surveyName);
+	
+	public List<UserAnswer> findBySurveyNameAndUserId(String surveyName, String userId);
 }
